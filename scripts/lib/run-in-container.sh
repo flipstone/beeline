5545ee24e04cebd2182a924033fa174381591107
David Vollbracht <david@flipstone.com>
2024-02-16 12:46:45 -0600

    Bumps `shrubbery` dependency for `0.2` compatibility

--- scripts/lib/run-in-container.sh
+++ scripts/lib/run-in-container.sh
@@ -2,6 +2,6 @@ if [ "$IN_DEV_CONTAINER" ]; then
   # Already in container, nothing to do
   :
 else
-  docker-compose build
-  exec docker-compose run dev $0 "$@"
+  docker compose build
+  exec docker compose run dev $0 "$@"
 fi
